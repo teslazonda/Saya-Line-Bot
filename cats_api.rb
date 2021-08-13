b2cb87c602852cb152774e8e7f6fb3f9f4f0e75e
Quill Cronwall <quilljazz@gmail.com>
2021-08-13 21:30:15 +0900

    commenting out unfinished api code

--- cats_api.rb
+++ cats_api.rb
@@ -1,11 +1,11 @@
-require 'date'
-require 'json'
-require 'open-uri'
+# require 'date'
+# require 'json'
+# require 'open-uri'
 
-def fetch_cat_pic
-  url = 'https://api.thecatapi.com/v1/images/search'
- cat_image_message = JSON.parse { "type": "image", 
-    "originalContentUrl": url,
-    "previewImageUrl":"https://api.thecatapi.com/v1/images/search" 
-  }
-end
+# def fetch_cat_pic
+#   url = 'https://api.thecatapi.com/v1/images/search'
+#  cat_image_message = JSON.parse { "type": "image",
+#     "originalContentUrl": url,
+#     "previewImageUrl":"https://api.thecatapi.com/v1/images/search"
+#   }
+# end
